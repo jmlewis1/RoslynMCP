46bf21be060a16b85954ca269d0eed257a99fe87
Justin <jmlewis1@hotmail.com>
2025-05-23 11:00:34 -0700

    Extending TestSln

---
+++ TestSln/TestProject/NS/NSClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.NS
+{
+    public class NSClass
+    {
+        public TestClass TestClass { get; set; } = new TestClass();
+        public NSClass() { }
+
+        public async Task FuncAsync()
+        {
+            Person person = new Person();
+            TestClass.person = person;
+        }
+    }
+}
---
+++ TestSln/TestProject/TestClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class TestClass
+    {
+        public Person person { get; set; } = new Person();
+
+        public int aNum { get; set; }
+    }
+}
